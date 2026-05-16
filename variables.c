b3d086f92d13f2f8e43235132308f721aeff41fd
itsuki3102pj22 <i.matsu3102@gmail.com>
2026-05-16 18:34:40 +0900

    配列の基礎

--- variables.c
+++ variables.c
@@ -2,11 +2,26 @@
 
 #include <stdio.h>
 
+void hello(void)
+{
+    int a;
+    a = 20;
+    printf("%d\n", a);
+}
+
+int a = 42;
+
 int main(void)
 {
-    const int a = 42;
-    a = 50; /* constで固定しているのでエラーになる*/
+    /*const int a = 42;
+    a = 50; //constで固定しているのでエラーになる
     printf("%d\n", a);
+    hello();
+    */
+
+    char c;
+    c = 66;
+    printf("%c\n", c);
 
     return(0);
 }
